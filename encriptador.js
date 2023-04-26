2f73d6d4aef0414392d864a8f29f10da49580000
ElMemoGG <omem.emanuel@gmail.com>
2023-04-25 18:43:47 -0600

    fix scroll

--- encriptador.js
+++ encriptador.js
@@ -13,7 +13,17 @@ const loading1 = document.getElementById("loading1");
 const loading2 = document.getElementById("loading2"); 
 
 
+/* const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
 
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+}
+ */
 
 const handleCopy = (text)=>{
 
